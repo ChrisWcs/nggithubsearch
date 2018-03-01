d9a69a2c63593783ca499b69baf88c914ab28416
Christian <ChrisWcs@users.noreply.github.com>
2018-03-01 14:03:57 -0700

    Add components to plan app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserService } from './user.service';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { UserContentComponent } from './user-content/user-content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchInputComponent,
+    UserContentComponent
   ],
   imports: [
     BrowserModule
